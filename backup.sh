7c7d15137670d7bab6e1df77f7f894f18031711a
Daniel Nägele <code@danieln.de>
2018-07-10 13:58:02 +0200

    add proxy detector script

--- backup.sh
+++ backup.sh
@@ -2,11 +2,11 @@
 read -s -p "enter password: " pass
 echo
 
-BORG_PASSPHRASE=$pass BORG_RSH="ssh -i ~/.ssh/id_danieln" borg create -v --stats --progress --one-file-system \
-    --compression zlib,5 \
-    -e 'sh:/home/danieln/Desktop' \
-    -e 'sh:/home/danieln/Downloads' \
-    --exclude-from '/home/danieln/scripts/EXCLUDE' \
-    16385@ch-s010.rsync.net:borg::'{hostname}-{now:%Y-%m-%d}' /home/danieln/ /home/danieln/.config/ /home/danieln/.gnupg/
+BORG_PASSPHRASE=$pass borg create -v \
+    --stats --progress --one-file-system --compression zlib,5 \
+    16385@ch-s010.rsync.net:backup::'{hostname}-{now:%Y-%m-%d}' \
+    /home/danieln/work /home/danieln/code \
+    /home/danieln/documents /home/danieln/logbook \
+    /home/danieln/notes /home/danieln/.gnupg/ # /home/danieln/pictures
 
-BORG_PASSPHRASE=$pass BORG_RSH="ssh -i ~/.ssh/id_danieln" borg prune -v 16385@ch-s010.rsync.net:borg --prefix '{hostname}-' --keep-daily=7 --keep-weekly=4 --keep-monthly=6
+BORG_PASSPHRASE=$pass borg prune -v 16385@ch-s010.rsync.net:backup --prefix '{hostname}-' --keep-daily=7 --keep-weekly=4 --keep-monthly=6
---
+++ need-proxy.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+! nmcli c show --active | grep "BelWue NOC"
